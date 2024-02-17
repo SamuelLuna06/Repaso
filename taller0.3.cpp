6ca90049dfa66912d3ef74e9de80f70388c84999
Samuel Luna <134666286+SamuelLuna06@users.noreply.github.com>
2024-02-16 21:00:20 -0500

    Update taller0.3.cpp

--- taller0.3.cpp
+++ taller0.3.cpp
@@ -6,12 +6,14 @@ int main(){
 
     int sueldoTrabajador; 
     int nuevoSueldo;
+    int aumento;
 
     cout << "Ingrese su sueldo: " << endl;
     cin >> sueldoTrabajador;
 
     if(sueldoTrabajador < 1000000){
-        nuevoSueldo = 1000000;
+        aumento = sueldoTrabajador * 0.15;
+        nuevoSueldo = sueldoTrabajador + aumento;
         cout << "Su nuevo sueldo es de " << nuevoSueldo << endl;
     }else{
         cout << "Su sueldo no es apto para un aumento." << endl;
